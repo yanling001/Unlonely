db026b59b83227a3de21f1dbee4d54cfe1f7ac00
yasnling001 <1964074623@qq.com>
2020-08-03 16:59:04 +0800

    first commit

---
+++ src/test/java/com/hongyaoz/unlonely/UnlonelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongyaoz.unlonely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlonelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
