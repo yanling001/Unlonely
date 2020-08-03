ec270c4a1f72a8c98cb8183f01221e17d3c96044
yasnling001 <1964074623@qq.com>
2020-08-03 17:41:51 +0800

    useræ登录¥口

---
+++ unlonely-umps/src/main/java/com/hongyaoz/unlonelyumps/UnlonelyUmpsApplication.java
@@ -0,0 +1,13 @@
+package com.hongyaoz.unlonelyumps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlonelyUmpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnlonelyUmpsApplication.class, args);
+    }
+
+}
---
+++ unlonely-umps/src/test/java/com/hongyaoz/unlonelyumps/UnlonelyUmpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongyaoz.unlonelyumps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlonelyUmpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
